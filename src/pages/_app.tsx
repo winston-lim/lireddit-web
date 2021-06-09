0a3844b9737d95ae29506d4c2fdca85541c1674e
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-09 16:16:19 +0800

    Update _app to use urql client and register cache updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,96 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import {
+	LoginMutation,
+	LogoutMutation,
+	MeDocument,
+	MeQuery,
+	RegisterMutation,
+} from "../generated/graphql";
 
-import theme from '../theme'
+import theme from "../theme";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
-  )
+function typedUpdateQuery<Result, Query>(
+	cache: Cache,
+	qi: QueryInput,
+	result: any,
+	fn: (r: Result, q: Query) => Query
+) {
+	return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
 }
 
-export default MyApp
+const client = createClient({
+	url: "http://localhost:4000/graphql",
+	fetchOptions: {
+		credentials: "include",
+	},
+	exchanges: [
+		dedupExchange,
+		cacheExchange({
+			updates: {
+				Mutation: {
+					login: (_result, args, cache, info) => {
+						typedUpdateQuery<LoginMutation, MeQuery>(
+							cache,
+							{ query: MeDocument },
+							_result,
+							(result, query) => {
+								if (result.login.errors) {
+									return query;
+								} else {
+									return {
+										me: result.login.user,
+									};
+								}
+							}
+						);
+					},
+					register: (_result, args, cache, info) => {
+						typedUpdateQuery<RegisterMutation, MeQuery>(
+							cache,
+							{ query: MeDocument },
+							_result,
+							(result, query) => {
+								if (result.register.errors) {
+									return query;
+								} else {
+									return {
+										me: result.register.user,
+									};
+								}
+							}
+						);
+					},
+					logout: (_result, args, cache, info) => {
+						typedUpdateQuery<LogoutMutation, MeQuery>(
+							cache,
+							{ query: MeDocument },
+							_result,
+							() => ({ me: null })
+						);
+					},
+				},
+			},
+		}),
+		fetchExchange,
+	],
+});
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<Provider value={client}>
+			<ChakraProvider resetCSS theme={theme}>
+				<ColorModeProvider
+					options={{
+						useSystemColorMode: true,
+					}}
+				>
+					<Component {...pageProps} />
+				</ColorModeProvider>
+			</ChakraProvider>
+		</Provider>
+	);
+}
+
+export default MyApp;
