3ac53c1e3eb757a641967f112dd095f210747cd9
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-09 16:17:59 +0800

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
-const Index = () => <div>Hello world</div>;
+import { NavBar } from "../components/NavBar";
+
+const Index = () => (
+	<>
+		<NavBar />
+		<div>hello</div>
+	</>
+);
 
 export default Index;
