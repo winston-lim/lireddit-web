490f0b0b6d38dad36a84b156fd75630225466e96
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-11 10:23:48 +0800

    Update InputField to conditionally render Textarea

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,19 +1,20 @@
-import { Box } from '@chakra-ui/layout';
-import React from 'react';
+import { Box } from "@chakra-ui/layout";
+import React from "react";
 
+export type WrapperVariant = "small" | "regular";
 interface wrapperProps {
-	variant?: 'small' | 'regular';
+	variant?: "small" | "regular";
 }
 
 export const Wrapper: React.FC<wrapperProps> = ({
 	children,
-	variant = 'regular',
+	variant = "regular",
 }) => {
 	return (
 		<Box
 			mt={8}
 			mx="auto"
-			maxW={variant === 'regular' ? '800px' : '400px'}
+			maxW={variant === "regular" ? "800px" : "400px"}
 			w="100%"
 		>
 			{children}
