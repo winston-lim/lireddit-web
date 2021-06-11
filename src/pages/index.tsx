2ff2e43fd70b03f3911d9e530bcc831f14dd333b
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-11 10:27:24 +0800

    Update index and changePassword pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,24 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/create-urql-client";
 import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
 
 const Index = () => {
 	const [{ data }] = usePostsQuery();
 	return (
-		<>
-			<NavBar />
+		<Layout>
+			<NextLink href="/create-post">
+				<Link>create post</Link>
+			</NextLink>
 			<div>hello</div>
 			{!data ? (
 				<div>...loading</div>
 			) : (
 				data.posts.map((p) => <div key={p.id}>{p.title}</div>)
 			)}
-		</>
+		</Layout>
 	);
 };
 
